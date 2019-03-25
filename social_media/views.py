1e3c825099106fd28e419a10668bd49def28a2ad
Wihogora Wivine <wwihogora@gmail.com>
2019-03-25 12:44:08 +0200

    Add function of comment in views.py

--- social_media/views.py
+++ social_media/views.py
@@ -15,3 +15,25 @@ def index(request):
 
     print(image_posts)
     return render(request, 'index.html', {"title":title,"image_posts":image_posts})
+
+
+# @login_required(login_url='/accounts/login/')
+def comment(request,id):
+	
+	post = get_object_or_404(Image,id=id)	
+	current_user = request.user
+	print(post)
+
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.user = current_user
+			comment.image = post
+			comment.save()
+			return redirect('index')
+	else:
+		form = CommentForm()
+
+	return render(request,'comment.html',{"form":form})  
