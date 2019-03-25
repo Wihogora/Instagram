cff877f08f3fcadc25a344420d8d2ec67d08ce1c
Wihogora Wivine <wwihogora@gmail.com>
2019-03-25 10:53:09 +0200

    first commit

---
+++ social_media/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Profile, Image,Comments,Follow, Unfollow, Likes
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comments)
+admin.site.register(Follow)
+admin.site.register(Unfollow)
+admin.site.register(Likes)
